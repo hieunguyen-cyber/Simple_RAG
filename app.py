248c239cf4441bbe133eeab1c82aba934986e6b2
Hieu <hieuasroma@gmail.com>
2025-05-01 01:25:23 +0700

    "L"

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 import streamlit as st
 from src.chatbot import RestaurantChatbot
+import os
+import requests
 
+
+model_file = "gemma-2-2b-it-Q4_K_M.gguf"
+local_path = "models"
+model_path = os.path.join(local_path, model_file)
+if not os.path.exists(model_path):
+    url = "https://drive.usercontent.google.com/download?id=1XOhWiIEpXccO5cTFXakt0tUINyQNnG7w&export=download&authuser=0&confirm=t&uuid=c4470895-3d14-43b8-af6d-267f146abedb&at=APcmpowYmv7x8M0fJvmfp-djUnZb:1746033199358"
+    output_file = "gemma-2-2b-it-Q4_K_M.gguf"
+
+    headers = {
+        "Host": "drive.usercontent.google.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
+        "Cookie": "SID=g.a000wQhn4l...; __Secure-1PSID=...; ..."  # Cookie rút gọn. Thay bằng bản đầy đủ nếu cần xác thực
+    }
+
+    response = requests.get(url, headers=headers, stream=True)
+
+    with open(output_file, "wb") as f:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+
+    print(f"Tải xong file: {output_file}")
+    # Tạo thư mục models nếu chưa có
+    os.makedirs('./models', exist_ok=True)
+
+    # Di chuyển file vào thư mục ./models
+    os.rename(model_file, f'./{local_path}/{model_file}')
+    
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
